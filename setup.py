bfb11b97788287b9ac0a8c9a4396b13bfa69d916
U-LUNDALOGIK\jeg <jeg@1138M32.lundalogik.local>
2015-11-28 17:09:00 +0100

    Add dotfiles

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from os.path import (join, realpath, abspath, basename, exists,
+                     expanduser, isdir, realpath, dirname)
+
+
+dotfiles = [
+    '.gitconfig',
+    '.vimrc',
+    '.minttyrc',
+    '.tmux.conf',
+    '.zshrc',
+]
+
+
+paths = {
+	'home': expanduser('~'),
+	'dot_repo': realpath(dirname(__file__))
+}
+
+
+def link(src, dest):
+    if not exists(src):
+        raise ValueError("{} does not exist".format(src))
+
+    if os.path.islink(dest):
+        os.unlink(dest)
+
+    try:
+        os.symlink(src, dest)
+        print("{} -> {}: linked".format(src, dest))
+    except Exception as e:
+        print("Error occured: {}".format(e))
+
+
+if __name__ == '__main__':
+    for file in dotfiles:
+        src = join(paths['home'], file)
+        dest = join(paths['dot_repo'], file)
+        link(src, dest)
