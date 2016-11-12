60cb3ea38743c802de440aad5af6d425681e03a1
Jens Gustafsson <jens.gustafsson@lundalogik.se>
2016-11-12 12:08:49 +0100

    Update tmux script to work with sessions instead of windows.

--- .zsh/custom_tmux.zsh
+++ .zsh/custom_tmux.zsh
@@ -1,74 +1,52 @@
-tma() {
-    tmux new-session -t $1
-}
-
-tmx_window_exist() {
+# Check if a window with a specifix name exists or not in the active session.
+tmux_window_exist() {
     local PROJECT_NAME=$1
-    local SESSION_NAME=$2
-    tmux list-windows -t $SESSION_NAME | grep "^[[:digit:]]\+: $PROJECT_NAME.\? " &> /dev/null
+    tmux list-windows | grep "^[[:digit:]]\+: $PROJECT_NAME.\? " &> /dev/null
     if [ $? != 0 ]; then
         return 1
     else
         return 0
     fi
 }
-
-tmxa() {
-    local SESSION_NAME=$1
-    local PROJECT_NAME=$2
-    local PROJECT_PATH=$3
-    local WINDOW_NAME=$PROJECT_NAME
-
-    if [ -z $SESSION_NAME ]; then
-        echo "ERROR: Rerun the command with the session-name as the first argument"
-        return 1
-    fi
-
-    if [ -z $PROJECT_NAME ]; then
-        echo "ERROR: Rerun the command with the project-name as the second argument"
-        return 1
-    fi
-
-    if [ -z $PROJECT_PATH ]; then
-        echo "ERROR: Rerun the command with the project-path as the third argument"
-        return 1
-    fi
-
-    tmux has-session -t $SESSION_NAME &> /dev/null
-    if [ $? != 0 ]; then
-        echo "Session does not exist, creating..."
-        if [ -z $PROJECT_NAME ]; then
-            echo "ERROR: Rerun the command with the project name as the second argument"
+ 
+# Create a new session or attach to an exisiting session
+# Path to where session shall be created is optional. If provided,
+# use the basename as session name.
+tms() {
+    if [ -n "$1" ]; then
+        local CURRENT_PATH="`pwd`"
+        cd $1 &> /dev/null
+        if [ $? != 0 ]; then
+            echo 'The provided path does not exist!'
             return 1
         fi
-        tmux new-session -s $SESSION_NAME -d -n $WINDOW_NAME -c $PROJECT_PATH
-        tmux send-keys -t $WINDOW_NAME "av && clear" C-m
+        local PROJECT_PATH="`pwd`"
+        local SESSION_NAME=$(`echo basename $PROJECT_PATH`)
+        cd $CURRENT_PATH
     fi
 
-    tmx_window_exist $WINDOW_NAME $SESSION_NAME
-    if [ $? != 0 ]; then
-        echo "Creating new window for $PROJECT_NAME project..."
-        tmux new-window -n $WINDOW_NAME -c $PROJECT_PATH
-        echo "Activating python venv if it exists in project folder"
-        tmux send-keys -t $WINDOW_NAME "av && clear" C-m
+    if [ -n "$TMUX" ]; then
+        if [ -n "$SESSION_NAME" ]; then
+            tmux switch-client -t $SESSION_NAME &> /dev/null
+            if [ $? != 0 ]; then
+                tmux new -s $SESSION_NAME -d -c $PROJECT_PATH && tmux switch-client -t $SESSION_NAME
+            fi
+        else
+            return 1
+        fi
+
     else
-        tmux select-window -t $WINDOW_NAME
+        if [ -n "$SESSION_NAME" ]; then
+            tmux attach -t $SESSION_NAME || tmux new-session -s $SESSION_NAME -c $PROJECT_PATH
+        else
+            tmux attach
+        fi
     fi
-
-    tmux attach-session -t $SESSION_NAME &> /dev/null
-    return 0
 }
 
-tmx_lime() {
-    local SESSION_NAME="TMX"
-    local PROJECT_NAME=$1
-    local PROJECT_PATH="/home/jeg/development/lundalogik/pro/"
-    if [ -z $PROJECT_NAME ]; then
-        PROJECT_NAME="tng"
-    fi
-    tmxa $SESSION_NAME $PROJECT_NAME $PROJECT_PATH$PROJECT_NAME
-}
-
+# Open a new window inside a session.
+# If a path is provided, open the window and change dir to the provided
+# path. Use the basename as window-name.
 tmx() {
     local CURRENT_PATH="`pwd`"
     if [ -n $1 ]; then
@@ -76,7 +54,13 @@ tmx() {
         local PROJECT_PATH="`pwd`"
         cd $CURRENT_PATH
     fi
-    local SESSION_NAME="TMX"
     local PROJECT_NAME=$(`echo basename $PROJECT_PATH`)
-    tmxa $SESSION_NAME $PROJECT_NAME $PROJECT_PATH
+
+    tmux_window_exist $PROJECT_NAME &> /dev/null
+    if [ $? != 0 ]; then
+        tmux new-window -n $PROJECT_NAME -c $PROJECT_PATH
+    else
+        tmux select-window -t $PROJECT_NAME
+    fi
+    return 0
 }
