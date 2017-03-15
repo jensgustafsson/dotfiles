287ff793b9346fe99175d665e19a41504708b511
Jens Gustafsson <jens.gustafsson@lundalogik.se>
2017-03-15 08:02:47 +0100

    Add alias for changing between bg1,bg2,bg3.

--- .zsh/base16.zsh
+++ .zsh/base16.zsh
@@ -1,5 +1,8 @@
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
-base16_chalk
+alias bg1=base16_chalk
+alias bg2=base16_solarized-light
+alias bg3=base16_materia
+bg3
 
